c92ba01604097bff37701d5afbb410a86c9c43d1
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-22 18:00:30 +0100

    Add provider, fix files structure and imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'C:/Users/Wu/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react';
-import ReactDOM from 'C:/Users/Wu/AppData/Local/Microsoft/TypeScript/2.9/node_modules/@types/react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+    <Provider store={store}>
+        <h1>Inicjalizacja projektu</h1>
+    </Provider>,
+    document.getElementById('root')
+);
