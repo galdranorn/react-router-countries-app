9147615bcb5f0f04509c09f45ee529090ea1eac3
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-27 15:37:57 +0100

    Add countries details functionality

--- src/routes.js
+++ src/routes.js
@@ -3,13 +3,17 @@ import { Route, IndexRoute } from 'react-router';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component';
 import CountryFlagContainer from './containers/flag-container.component';
+import CountryDetailsContainer from './containers/country-details.container.component';
 import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component';
 
 export default (
     <Route path='/' component={Navigation}>
         <IndexRoute component={Home}/>
-        <Route path='countries' component={CountryFlagContainer}/>
+        <Route path='countries' >
+            <IndexRoute component={CountryFlagContainer}/>
+            <Route path='country/:id' component={CountryDetailsContainer}/>
+        </Route>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={NotFound}/>
     </Route>
