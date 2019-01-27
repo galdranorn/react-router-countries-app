00b6451f291e780cce84b78022e728b2732bf366
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-27 15:18:46 +0100

    Fix disappearing nav

--- src/presentational/navigation.component.js
+++ src/presentational/navigation.component.js
@@ -5,20 +5,22 @@ import '../../node_modules/bootstrap/dist/css/bootstrap-theme.min.css';
 
 const Navigation = props => (
     <div>
-        <nav className="navbar navbar-default">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" to="/">Państwa.js</Link>
-                </div>
-                <div className="collapse navbar-collapse">
-                    <ul className="nav navbar-nav">
-                        <li><Link to="/countries" class="nav-link">Countries</Link></li>
-                        <li><Link to="/continents" class="nav-link">Continents</Link></li>
-                        <li><Link to="/contact" class="nav-link">Contact</Link></li>
-                    </ul>
-                </div>
-            </div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <Link className="navbar-brand" to="/">Państwa.js</Link>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="nav navbar-nav">
+              <li><Link to="/countries" class="nav-link">Countries</Link></li>
+              <li><Link to="/continents" class="nav-link">Continents</Link></li>
+              <li><Link to="/contact" class="nav-link">Contact</Link></li>
+            </ul>
+          </div>  
+
         </nav>
+
         <div className="container-fluid">
             {props.children}
         </div>
