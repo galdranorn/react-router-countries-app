83515ac6cc7822ba1776cfe179a53ff9446e7eb2
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-01-27 15:28:04 +0100

    Add countries list functionality

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import Navigation from './presentational/navigation.component';
 import Home from './presentational/home.component';
+import CountryFlagContainer from './containers/flag-container.component';
 import Contact from './presentational/contact.component';
 import NotFound from './presentational/not-found.component';
 
 export default (
     <Route path='/' component={Navigation}>
         <IndexRoute component={Home}/>
+        <Route path='countries' component={CountryFlagContainer}/>
         <Route path='contact' component={Contact}/>
         <Route path='*' component={NotFound}/>
     </Route>
