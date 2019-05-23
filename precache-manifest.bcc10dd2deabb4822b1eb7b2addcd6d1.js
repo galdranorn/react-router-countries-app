9b0177c3e3e931c3f23beef489b05f9dfc400ddd
Justyna Skiberowska <skiberowska.justyna@wp.pl>
2019-05-23 21:33:51 +0200

    Updates

---
+++ precache-manifest.bcc10dd2deabb4822b1eb7b2addcd6d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d295e9e163cb20939e6",
+    "url": "/react-router-countries-app/static/js/runtime~main.3d295e9e.js"
+  },
+  {
+    "revision": "20d99395e138ef634b55",
+    "url": "/react-router-countries-app/static/js/main.20d99395.chunk.js"
+  },
+  {
+    "revision": "eb6430d9907edaed6a64",
+    "url": "/react-router-countries-app/static/js/1.eb6430d9.chunk.js"
+  },
+  {
+    "revision": "20d99395e138ef634b55",
+    "url": "/react-router-countries-app/static/css/main.4f31576e.chunk.css"
+  },
+  {
+    "revision": "eb6430d9907edaed6a64",
+    "url": "/react-router-countries-app/static/css/1.166d4b25.chunk.css"
+  },
+  {
+    "revision": "0cab7d7cb629a9d90fe1d7feaffe60fc",
+    "url": "/react-router-countries-app/index.html"
+  }
+];
